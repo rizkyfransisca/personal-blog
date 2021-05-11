4d13838b2bd69193096db98c4b6e08158fca816a
rizkyfransisca <rizky.royal@gmail.com>
2021-05-11 11:06:57 +0700

    test oauth

--- routes/blogs.js
+++ routes/blogs.js
@@ -33,6 +33,9 @@ router.post('/send-mail',async(req,res)=>{
     const accessToken = await oAuth2Client.getAccessToken()
     let transporter = nodemailer.createTransport({
       service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
           type: 'OAuth2',
           user: process.env.EMAIL,
